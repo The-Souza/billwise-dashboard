1fce0ab60c0c8ec240b1c043996979b9731db98c
Guilherme Campos <guilhermev272@gmail.com>
2026-02-09 17:39:23 -0300

    feat(ThemeProvider): add toggle theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
+import { ThemeProvider } from "@/providers/ThemeProvider";
 import { inter, poppins } from "./fonts";
 import "./globals.css";
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html
       lang="pt-BR"
@@ -13,9 +12,16 @@ export default function RootLayout({
       className={`${inter.variable} ${poppins.variable} antialiased`}
     >
       <body className="min-h-screen flex flex-col font-sans">
-        <main className="flex-1 px-6 py-6 sm:px-10 flex flex-col items-center">
-          {children}
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="flex-1 px-6 py-6 sm:px-10 flex flex-col items-center">
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
