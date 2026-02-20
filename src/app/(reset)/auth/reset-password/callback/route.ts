67f85bfaafd83a8e390c9da8ae509210f9ddba49
Guilherme Campos <174835117+The-Souza@users.noreply.github.com>
2026-02-19 21:03:47 -0300

    feat(callback): add reset password callback

---
+++ src/app/(reset)/auth/reset-password/callback/route.ts
@@ -0,0 +1,16 @@
+import { createServerSupabase } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
+
+  if (code) {
+    const supabase = await createServerSupabase();
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  return NextResponse.redirect(
+    new URL("/auth/reset-password/update", request.url),
+  );
+}
