b4603fe2f6aada57527a7b3bc0036ab74f899fc2
Guilherme Campos <174835117+The-Souza@users.noreply.github.com>
2026-02-19 21:01:47 -0300

    fix(privateLayout): fix text

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,7 +1,7 @@
 import { createServerSupabase } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 
-export default async function PrivateLayput({
+export default async function PrivateLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const supabase = await createServerSupabase();
