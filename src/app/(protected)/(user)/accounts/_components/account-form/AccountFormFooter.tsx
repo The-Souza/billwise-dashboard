17c412b961ba5243901adc2cf7258175b774244c
Guilherme Campos <174835117+The-Souza@users.noreply.github.com>
2026-04-01 19:58:43 -0300

    fix: fix of the loading state

--- src/app/(protected)/(user)/accounts/_components/account-form/AccountFormFooter.tsx
+++ src/app/(protected)/(user)/accounts/_components/account-form/AccountFormFooter.tsx
@@ -4,10 +4,14 @@ import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/ui/spinner";
 import { SaveIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useFormState } from "react-hook-form";
 import { useAccountForm } from "./AccountFormContext";
 
 export function AccountFormFooter() {
   const { form, isEditing } = useAccountForm();
+  const { isSubmitting } = useFormState({
+    control: form.control,
+  });
   const router = useRouter();
 
   return (
@@ -15,12 +19,17 @@ export function AccountFormFooter() {
       <Button
         type="button"
         variant="outline"
+        className="transition-transform ease-in hover:scale-103 active:scale-97"
         onClick={() => router.push("/accounts")}
       >
         Cancelar
       </Button>
-      <Button type="submit" disabled={form.formState.isSubmitting}>
-        {form.formState.isSubmitting ? (
+      <Button
+        type="submit"
+        className="transition-transform ease-in hover:scale-103 active:scale-97"
+        disabled={isSubmitting}
+      >
+        {isSubmitting ? (
           <Spinner data-icon="inline-start" />
         ) : (
           <SaveIcon className="h-4 w-4" />
