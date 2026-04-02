8aa290682e978d6311655792402e23b4e06394ca
Guilherme Campos <174835117+The-Souza@users.noreply.github.com>
2026-04-02 18:12:19 -0300

    fix: fix time zone formatting for due date

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,4 +1,6 @@
 export const formatDate = (dateString: string) =>
-  new Intl.DateTimeFormat("pt-BR", { day: "2-digit", month: "short" }).format(
-    new Date(dateString),
-  );
+  new Intl.DateTimeFormat("pt-BR", {
+    day: "2-digit",
+    month: "short",
+    timeZone: "UTC",
+  }).format(new Date(dateString));
