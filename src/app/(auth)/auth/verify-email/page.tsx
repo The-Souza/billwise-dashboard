40c855d5b8c53483ef533f3bf9672a4346863b41
Guilherme Campos <174835117+The-Souza@users.noreply.github.com>
2026-05-22 16:02:01 -0300

    feat(seo): update og image, description and add robots rules

--- src/app/(auth)/auth/verify-email/page.tsx
+++ src/app/(auth)/auth/verify-email/page.tsx
@@ -1,7 +1,12 @@
 import { Card } from "@/components/ui/card";
 import { createServerSupabase } from "@/lib/supabase/server";
+import type { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { VerifyEmail } from "./_components/VerifyEmail";
+
+export const metadata: Metadata = {
+  robots: { index: false, follow: false },
+};
 
 export default async function VerifyEmailPage({
   searchParams,
